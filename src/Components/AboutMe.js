0c7236f72e03994fa634907e043c397c2785ec7d
Veny <veny@mag.net.id>
2025-08-25 11:56:51 +0700

    About Me

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { AppText } from '../Constants'
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <div>
+      <div className='h-[300px] bg-purple-600 item-center w-full'>
+        <h2 className='text-[50px] text-center font-bold text-white'>{AppText.About} <span className='text-black'>{AppText.Me}</span>  </h2>
+        <h2 className='text-white mt-10'>{AppText.aboutMeDescripion}</h2>
+      </div>
+    </div>
   )
 }
 
--- src/Constants/index.js
+++ src/Constants/index.js
@@ -7,6 +7,8 @@ export const AppText={
     hello:'Hello,',
     Iam:'I am ',
     RahulSanap:'Rahul Sanap',
+    About:'About ',
+    Me:'Me',
     aboutMeDescripion:'I am software developer working in IT industry from  last 6 Years as Full stack developer. Expert in UI/UX design, Front-end  and Back-end Development',
     Skills:'Skills & ',
     Experties:'Experties',
